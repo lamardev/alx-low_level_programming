3aefc9b46391b7350c29390f35abebbaecf84251
lamardev <babashehushettima@outlook.com>
2024-01-11 23:22:00 +0100

    Fixed betty coding style

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -4,14 +4,14 @@
  * free_dlistint - Frees a doubly linked list.
  * @head: Pointer to the head node of the list.
  *
- * Description: This function frees the memory occupied by a doubly linked list.
+ * Description: frees the memory occupied by a doubly linked list.
  * Return: The function does not return a value.
  */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp = NULL;
 
-	while(head != NULL)
+	while (head != NULL)
 	{
 		tmp = head->next;
 		free(head);
