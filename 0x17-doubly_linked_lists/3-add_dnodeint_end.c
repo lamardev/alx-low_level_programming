ef0960947a2060bd0a6487c58627126bb789481f
lamardev <babashehushettima@outlook.com>
2024-01-11 22:58:38 +0100

    Fixed betty codestyle

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,16 +1,17 @@
 #include "lists.h"
 
 /**
- * Adds a new node with the given value to the end of a doubly linked list.
+ * add_dnodeint_end - Adds a new node with its value to the end of a list.
  *
- * @param head: A double pointer to the head of the doubly linked list.
- * @param n: The integer value to add to the new node.
+ * @head: A double pointer to the head of the doubly linked list.
+ * @n: The integer value to add to the new node.
  *
- * @Return A pointer to the newly added node, or NULL if memory allocation fails.
+ * Return: pointer to the new node, or NULL if memory allocation fails.
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
+
 	if (new == NULL)
 		return (NULL);
 
@@ -26,6 +27,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	else
 	{
 		dlistint_t *tmp = *head;
+
 		while (tmp->next != NULL)
 		{
 			tmp = tmp->next;
